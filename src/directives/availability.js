dbc3c4f0d70449728222a9e26678c326a631eee6
Ivan Fraixedes <ifcdev@gmail.com>
2013-09-15 22:04:07 +0100

    Added yuidoc documentation tags

--- src/directives/availability.js
+++ src/directives/availability.js
@@ -2,6 +2,10 @@
  * @license Ivan Fraixedes Cugat
  * (c) 2013
  * License: MIT
+ *
+ * @module directives
+ * @namespace ifcDirectives
+ * @class Availability
  */
 
 (function (angular) {
@@ -10,30 +14,29 @@
 	var ifcDirModule = angular.module('ifcDirectives');
 
 	/**
-	 * @description
+	 *
 	 * Checks the server for a value to see if a valid value to use, or invalid. e.g. Invalid
 	 * some time is because it is a reserved word or it has to be unique in any collection
 	 *
-	 * This directive accepts the next settings parameters:
-	 *  -  ifc-availability: The itself directive attribute must have the url to send the request.
-	 *  - name: Used to name the parameter to send the value to check, if it doesn't exist, it uses by
-	 *  default 'value'.
-	 *  - pattern: Regular expression to check the input value, if it matches the regular expression,
-	 *    the directive sends a request to the server otherwise it doesn't send a request an
-	 *      invalidate the input form (if ngPattern exists, it is not needed to use that value because
-	 *      the ngPattern directive is execute before it and if it fails then this directive it is not
-	 *      executed)
-	 *  - additional-data: Object whose attributes will be sent to the server in each request.
-	 *  - id: The id attribute is used as prefix for the errors properties names reported, if it
-	 *    doesn't exist then 'availability' is used by default.
+	 * @method {String} ifc-availability The itself directive attribute must have the url to send
+	 *  the request.
+	 * @method {String} name Used to name the parameter to send the value to check. @default 'value'. I
+	 * @method {String} pattern Regular expression to check the input value, if it matches the regular
+	 *  expression, the directive sends a request to the server otherwise it doesn't send a request an
+	 *  invalidate the input form (if ngPattern exists, it is not needed to use that value because the
+	 *  ngPattern directive is execute before it and if it fails then this directive it is not
+	 *  executed).
+	 * @method {Object} additional-data Object whose attributes will be sent to the server in each
+	 *  request. If it contains an attribute with the same name that 'name' attribute, then it will
+	 *  be overridden by the input value.
+	 * @method {String} id The id attribute is used as prefix for the errors properties names
+	 *  reported, if it doesn't exist then 'availability' is used by default.
 	 *
-	 *  NOTE if the object contains an attribute with the same name that 'name' attribute, then
-	 *  it will be overridden by the input value.
-	 *
-	 * Errors reported, bear in mind that they are prepended by the `id` value as commented above:
-	 *   Unaccepted: when the slug doesn't pass the pattern
-	 *   Anavailable: when the server reply with not accepted slug response
-	 *   Uncheckable: If the request to the server return an error
+	 * @throws errors, bear in mind that they are prepended by the `id` attribute value as commented
+	 *    above:
+	 *    Unaccepted: when the slug doesn't pass the pattern
+	 *    Unavailable: when the server reply with not accepted slug response
+	 *    Uncheckable: If the request to the server return an error
 	 *
 	 */
 	ifcDirModule.directive('ifcAvailability', ['$timeout', '$http', function ($timeout, $http) {
@@ -74,18 +77,6 @@
 					});
 
 					postData = attrs.additionalData;
-
-//          scope.$watch(attrs.additionalData, function (newVal, oldVal) {
-//            if (angular.isObject(newVal)) {
-//              postData = newVal;
-//              additionalDataToSend = true;
-//            } else {
-//              additionalDataToSend = false;
-//              postData = {};
-//            }
-//          });
-//
-//          postData = scope.$eval(attrs.additionalData);
 				}
 
 				ctrl.$parsers.push(function (viewValue) {
