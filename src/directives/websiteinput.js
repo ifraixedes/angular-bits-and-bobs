3048470b5e2e014d600894d9c0b48b39d6ed4abb
Ivan Fraixedes <ifcdev@gmail.com>
2013-09-15 12:16:46 +0100

    Improved websiteUrl directive to manage required constraint properly and added example to use it with required constrain

--- src/directives/websiteinput.js
+++ src/directives/websiteinput.js
@@ -17,8 +17,9 @@
 	 * with the protocol part, when the introduced value is a right web site url, so it adds the
 	 * `http` protocol when the input url lacks of protocol.
 	 *
-	 * The directive report an error to the ngModelCtrl labeled websiteUrl, which is a boolean with
-	 * true value when the input text is a wrong url
+	 * The directive report an error to the ngModelCtrl labeled `websiteUrl` when the html element
+	 * doesn't have `name` attribute, otherwise prepend to it `WebsiteUrl`, which is a boolean with
+	 * true value when the input text is a wrong url.
 	 */
 	ifcDirModule.directive('ifcWebsiteInput', [
 		function () {
@@ -28,24 +29,28 @@
 				link: function (scope, elem, attrs, ngModelCtrl) {
 
 					var wsRegExp = /(?:(?:(https?):\/\/)?(?:[a-zA-Z]+[a-zA-Z0-9\-]+(?:\.[a-zA-Z0-9\-]+)*(?:\.[a-zA-Z]{2,}))(?:\/[\w$\-@.&+!*"'(),]*|%[0-9a-fA-F]*)*(?:\?(?:[\w$\-@.&+!*"'(),]|%[0-9a-fA-F])+)?(?:#(?:.*))?)/;
+					var varNameValiation = (attrs.id) ? attrs.id + 'WebsiteUrl' : 'websiteUrl';
 
 					ngModelCtrl.$parsers.push(parserAndFormatterFn);
 					ngModelCtrl.$formatters.push(parserAndFormatterFn);
 
 					function parserAndFormatterFn(value) {
 
-						var urlChecker = wsRegExp.exec(value);
+						var urlChecker;
+
+						if (!value) {
+							ngModelCtrl.$setValidity(varNameValiation, true);
+							return null;
+						}
+
+						urlChecker = wsRegExp.exec(value);
 
 						if (urlChecker === null) {
-							if ((!attrs.required) && (!value)) {
-								ngModelCtrl.$setValidity('websiteUrl', true);
-							} else {
-								ngModelCtrl.$setValidity('websiteUrl', false);
-							}
+							ngModelCtrl.$setValidity(varNameValiation, false);
 							return null;
 
 						} else {
-							ngModelCtrl.$setValidity('websiteUrl', true);
+							ngModelCtrl.$setValidity(varNameValiation, true);
 
 							if ((urlChecker[1] === undefined) && (value.length > 0)) {
 								return 'http://' + value;
